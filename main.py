ec594268c18bfe02bfaa4969818d8a6d9521d988
Zaitsev Georgiy <zaitsevgeorge.ru@gmail.com>
2026-08-15 19:54:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
+from PySide6.QtWidgets import QApplication
+from app.ui.main_window import MainWindow
 
-app = QApplication(sys.argv)
 
-window = QMainWindow()
-window.setWindowTitle("Street Planner - Test Window")
-window.resize(600, 400)
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
 
-label = QLabel("Test Window Text")
-window.setCentralWidget(label)
 
-window.show()
-sys.exit(app.exec())
+if __name__ == "__main__":
+    main()
